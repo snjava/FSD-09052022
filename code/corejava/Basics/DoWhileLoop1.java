e6c4616b5210d0674f6796ef70f0901d496ae65f
snjava <scnaikwadi2@gmail.com>
2022-06-07 08:06:56 +0530

    Looping Statement
    Looping Statement

---
+++ code/corejava/Basics/DoWhileLoop1.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop1
+{
+	public static void main(String ar[])
+	{
+		int i = 1;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<=10);
+	}
+}
---
+++ code/corejava/Basics/WhileLoop1.java
@@ -0,0 +1,28 @@
+public class WhileLoop1
+{
+	public static void main(String ar[])
+	{
+		int i = 1;
+		char c = 'A';
+		while(i<=26)
+		{
+			System.out.println(c++);	
+			++i; // i = i + 1;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
